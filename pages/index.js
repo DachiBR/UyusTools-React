c2c14953ebab9b835364927995272aaa923ab840
Dachi Bregadze <75274431+DachiBR@users.noreply.github.com>
2024-07-13 02:22:59 +0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,4 +0,0 @@
-export {default as About} from './about/About';
-export {default as Home} from './home/Home';
-export {default as Products} from './products/Products';
-export {default as Contact} from './contact/Contact';
