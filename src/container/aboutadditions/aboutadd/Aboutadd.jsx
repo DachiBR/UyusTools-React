e57fcf5082b7df9f00c9510171e9957ada13e5ad
Dachi Bregadze <75274431+DachiBR@users.noreply.github.com>
2024-08-01 11:15:21 +0400

    Update Aboutadd.jsx

--- src/container/aboutadditions/aboutadd/Aboutadd.jsx
+++ src/container/aboutadditions/aboutadd/Aboutadd.jsx
@@ -5,25 +5,28 @@ const Aboutadd = () => {
   return (
     <div className="gpt3__possibility section__padding" id="possibilty">
 
-    <div className="gpt3__possibility-content">
-      <h4>Shop with confidence at UyusTool </h4>
-      <h1 className="gradient__text">
-        Experience the quality and reliability of Uyustools today. 
-      </h1>
-      <p>
-       At Uyustools, we pride ourselves on providing users with reliable and durable tools.
-       Our comprehensive product line includes power tools, power tool accessories, hand tools, hardware tools, and more. 
-       Backed by an attentive service system encompassing quality control, packaging, storage, transportation, marketing support, 
-       and after-sales service, we've earned our reputation as a top company in the Chinese overseas tool industry.
-      </p>
-      <h4>
-      Request Early Access to Get Started
-      </h4>
-    </div>
+<div className="gpt3__possibility-content">
+  <h4> შეიძინეთ UyusTool-ში ნდობით </h4>
+  <h1 className="gradient__text">
+    გამოსცადეთ Uyustools-ის ხარისხი და სანდოობა დღესვე.
+  </h1>
+  <p>
+    Uyustools-ში ჩვენ ვამაყობთ ჩვენი მომხმარებლებისთვის სანდო და გამძლე ხელსაწყოების მიწოდებით. 
+    ჩვენი პროდუქციის ფართო ხაზი მოიცავს ელექტრო ხელსაწყოებს, ელექტრო ხელსაწყოების აქსესუარებს, 
+    ხელის ხელსაწყოებს, ტექნიკურ ხელსაწყოებს და სხვა. 
+    ხარისხის კონტროლი, შეფუთვა, შენახვა, ტრანსპორტირება, მარკეტინგის მხარდაჭერა და გაყიდვების 
+    შემდგომი სერვისის ჩათვლით, ყურადღებიანი მომსახურების სისტემით, ჩვენმა კომპანიამ მოიპოვა 
+    ჩინეთის ხელსაწყოების ინდუსტრიაში ლიდერის სახელი.
+  </p>
+  <h4>
+    მოითხოვეთ ადრეული წვდომა დასაწყებად
+  </h4>
+</div>
 
-    <div className="gpt3__possibility-image">
-      <img src={aboutImage} alt="possibility" srcset="" />
-    </div>
+<div className="gpt3__possibility-image">
+  <img src={aboutImage} alt="შესაძლებლობა" srcset="" />
+</div>
+
     
   </div>
   )
